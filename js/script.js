9ff865802f941d246221cd9107cdd1df8a3daa5d
Rakesh Shrestha <rakeshshrestha3222@gmail.com>
2021-04-25 19:41:20 +0545

    Changed Setinterval + Onclick buttons

--- js/script.js
+++ js/script.js
@@ -1,59 +1,50 @@
-const sliderImage = document.querySelector('.slider');
+
+// Setting an interval for slide show
+let index = 0;
+
+function makeSlideShow(slidesId){
+    const slides = document.getElementById(slidesId);
+    const slider = slides.querySelector('.slider');
+    const images = slider.querySelectorAll('img');
+
+    let index = 0;
+
+    setInterval(function() {
+        index += 1;
+        if (index === images.length){
+            index = 0;   
+        }
+        sliderImage.style.transform = `translateX(${index * -650}px)`;
+    }, 3500)
+}
+
+makeSlideShow('slides')
+
+
+//making button to change the picture of slider
+
 const buttons = document.querySelectorAll('.button');
-const numberofImage = document. querySelectorAll('.slider img').length;
+const sliderImage = document.querySelector('.slider');
+const numberofImage = document. querySelectorAll('.slider img').length;   
 
 let imageIndex = 1;
 let translateX = 0;
 
-// var i = 0;
-// var images = [];
-var time = 2000;
-
-//Image lists
-// images[0] = 'image1.jpg';
-// images[1] = 'image2.jpg';
-// images[2] = 'image3.jpg';
-// images[3] = 'image4.jpg';
-// images[4] = 'image5.jpg';
-
-function changeImg(){
-  
-    if(imageIndex !==1){
-        imageIndex--;
-        translateX += 650;
-    } else{
-        if(imageIndex !==numberofImage){
-            imageIndex ++;
-            translateX -= 650;
-        }
-    }
-    sliderImage.style.transform = `translateX(${translateX}px)`;
-    setTimeout("changeImg()", time);
-}
-
-window.onload = changeImg;
-
 
 buttons.forEach(button => {
     button.addEventListener('click', (e) =>{
-        setInterval(() => {
-
             if (e.target.id === 'previous'){
                 if(imageIndex !==1){
                     imageIndex--;
                     translateX += 650;
-                }
+                }   
             }
             else{
                 if(imageIndex !==numberofImage){
                     imageIndex ++;
                     translateX -= 650;
-                }
+                }   
             }
             sliderImage.style.transform = `translateX(${translateX}px)`;
-        }, 2000);
-       
-        
-
     });
 });
