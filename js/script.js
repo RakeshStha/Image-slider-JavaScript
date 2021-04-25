5839eafb134a9fbb0e4669c116a84f56c5341d98
Rakesh Shrestha <rakeshshrestha3222@gmail.com>
2021-04-25 18:03:51 +0545

    settimeout funciton

--- js/script.js
+++ js/script.js
@@ -5,11 +5,39 @@ const numberofImage = document. querySelectorAll('.slider img').length;
 let imageIndex = 1;
 let translateX = 0;
 
+// var i = 0;
+// var images = [];
+var time = 2000;
+
+//Image lists
+// images[0] = 'image1.jpg';
+// images[1] = 'image2.jpg';
+// images[2] = 'image3.jpg';
+// images[3] = 'image4.jpg';
+// images[4] = 'image5.jpg';
+
+function changeImg(){
+  
+    if(imageIndex !==1){
+        imageIndex--;
+        translateX += 650;
+    } else{
+        if(imageIndex !==numberofImage){
+            imageIndex ++;
+            translateX -= 650;
+        }
+    }
+    sliderImage.style.transform = `translateX(${translateX}px)`;
+    setTimeout("changeImg()", time);
+}
+
+window.onload = changeImg;
 
 
 buttons.forEach(button => {
     button.addEventListener('click', (e) =>{
         setInterval(() => {
+
             if (e.target.id === 'previous'){
                 if(imageIndex !==1){
                     imageIndex--;
